5a898d855942cacc2d5cff0a791e927872f559e9
Bushra Patel <bpatel60@yahoo.com>
2024-09-18 20:50:05 -0400

    Setup the backend and frontend templates using Vite with React and javascript

---
+++ drsmart/backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = 5000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
