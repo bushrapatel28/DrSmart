4fac50b9fff18c0b3c95364a9f9c081ba1147c37
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-09-21 19:27:53 -0400

    initial commit: after setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const port = 5000;
+const port = 6000;
 
 app.use(morgan('dev'));
 app.use(express.json());
