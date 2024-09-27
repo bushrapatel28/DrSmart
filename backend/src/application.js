720cab009581b03d8df1c0585e0f4faa7aa5de32
Bushra Patel <bpatel60@yahoo.com>
2024-09-26 20:04:08 -0400

    Add the application, index and environment setup files for backend. Install the react-datepicker library. Create Appointment component to show the calendar

---
+++ backend/src/application.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const app = express();
+
+const db = require("./db")
+
+const appointment = require("./routes/appointment");
+
+
+module.exports = function application(ENV) {
+
+  app.use(cors());
+  app.use(helmet());
+  app.use(bodyparser.json());
+  app.use(morgan('dev'));
+  app.use(express.json());
+  app.use(express.static('public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.use("/patient", appointment(db));
+
+  if (ENV === "development" || ENV === "test") {
+    Promise.all([
+      read(path.resolve(__dirname, `db/schema/create.sql`)),
+      read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
+    ])
+      .then(([create, seed]) => {
+        app.get("/debug/reset", (request, response) => {
+          db.query(create)
+            .then(() => db.query(seed))
+            .then(() => {
+              console.log("Database Reset");
+              response.status(200).send("Database Reset");
+            });
+        });
+      })
+      .catch(error => {
+        console.log(`Error setting up the reset route: ${error}`);
+      });
+  }
+
+  app.close = function() {
+    return db.end();
+  };
+
+  return app;
+};
