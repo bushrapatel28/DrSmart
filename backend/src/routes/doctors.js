53532fce530c009f03dd5129fe27c652717172af
Bushra Patel <bpatel60@yahoo.com>
2024-09-28 16:40:50 -0400

    Adjust the db query for fetching the doctors data

--- backend/src/routes/doctors.js
+++ backend/src/routes/doctors.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 
 module.exports = db => {
   router.get("/doctors", (req, res) => {
-    const protocol = req.protocol;
-    const host = req.hostname;
-    const port = process.env.PORT || 8001;
-    const serverUrl = `${protocol}://${host}:${port}`;
 
     db.query(`
       SELECT
@@ -18,13 +14,22 @@ module.exports = db => {
           'specialization', doctors.specialization,
           'date', availabilities.date,
           'vacant', availabilities.vacant,
-          'start_time', time_slots.start_time,
-          'end_time', time_slots.end_time
+          'availability', (
+            SELECT
+              json_agg(
+                json_build_object(
+                  'start_time', time_slots.start_time,
+                  'end_time', time_slots.end_time
+                )
+              )
+            FROM doctors
+            JOIN availabilities ON doctors.id = availabilities.doctor_id
+            JOIN time_slots ON availabilities.time_slot_id = time_slots.id
+          )
         )
       ) as doctor_data
       FROM doctors
-      JOIN availabilities ON doctors.id = availabilities.doctor_id
-      JOIN time_slots ON availabilities.time_slot_id = time_slots.id;
+      JOIN availabilities ON doctors.id = availabilities.doctor_id;
     `).then(({ rows }) => {
       res.json(rows[0].doctor_data);
     });
