e0bf75178e24a6e42e39e13183ec33a5cec4be1b
Bushra Patel <bpatel60@yahoo.com>
2024-09-28 01:27:32 -0400

    Create a DoctorList Component

---
+++ frontend/src/components/DoctorList/DoctorList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from 'react';
+
+const DoctorList = (props) => {
+  const [doctor, setDoctor] = useState("");
+  
+  
+}
+
+export default DoctorList;
